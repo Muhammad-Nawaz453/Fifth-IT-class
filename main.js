60e7e84515e031fc1b8d51ce30eaa1df2805ec48
Muhammad-Nawaz453 <157411548+Muhammad-Nawaz453@users.noreply.github.com>
2024-05-30 23:38:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+// let numberOfFaculty: number = 4;
+// let numberOfManagement: number = 3;
+// let numberOfAbsentFaculty = 2;
+// let totalNumberOfFaculty = numberOfFaculty * 21;
+// console.log(numberOfFaculty + numberOfManagement - numberOfAbsentFaculty);
+// console.log (20/84);
+// let firstName = "Nawaz";
+// let age = 17;
+// // let message = "Hello My name is: " + firstName;
+// // console.log(message);
+// let message = `hello my name is: ${firstName} and my age is: ${age}`;
+// console.log(message);
+// let index = 4;
+// ++index; 
+// index++;
+// ++index;
+// ++index;
+// ++index;
+// ++index;
+// console.log(index);
+// let index = 10;
+// let result = ++index + ++index + 8 + --index - 2;
+// console.log(result);
+// let num1 = 20;
+// num1 -= 22
+// console.log(num1)
+// let num1 = 20;
+// let num2 = 50;
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+let myAge = 17;
+if (myAge >= 18) {
+    console.log("You're allowed");
+}
+else {
+    console.log("you're not allowed");
+}
